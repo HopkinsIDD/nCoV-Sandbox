4e37106be21c82d2d256899abd00bc9e1e3adf78
edwardsjk <edwards.jk@gmail.com>
2020-01-24 19:56:06 -0500

    started gathering province level contextual data for stat model

---
+++ R/getContext.r
@@ -0,0 +1,39 @@
+# make contextual data for ncov19 outbreak
+library(sp)
+library(rgdal)
+library(raster)
+library(dplyr)
+
+#import temp monitor data (a sample for now, need to read more in)
+temp3 <- read.csv(file = "../data/context/2013051.csv")[, c(3, 4, 6, 11:14)]
+temp4 <- read.csv(file = "../data/context/2013054.csv")[, c(3, 4, 6, 11:14)]
+temp5 <- read.csv(file = "../data/context/2013024.csv")[, c(3, 4, 6, 27, 29, 31, 33)]
+
+#limit to february temps
+temps <- rbind(temp3, temp4, temp5)
+temps <- temps[substr(temps$DATE, 6, 7) == "02",]
+
+#get administration boundaries
+admU <-getData('GADM', country='CHINA', level=1)
+
+#get average temps in each province
+tempsdat2 <- data.frame(temps[, c(1:2,5)])
+tempsdat <- data.frame(temps[, c(1:2,5)])
+coordinates(tempsdat) <- c("LONGITUDE", "LATITUDE")
+proj4string(tempsdat) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+tempsdat2$prov <- over(tempsdat, admU)$NAME_1
+tempcsv <- aggregate(TAVG ~ prov, data = tempsdat2, FUN = mean)
+
+#estimate province areas in km^2
+admU$area_sqkm <- area(admU) / 1000000
+areacsv <- data.frame(prov=admU$NAME_1, area = admU$area_sqkm)
+tacsv <- merge(tempcsv, areacsv, by = "prov", all = TRUE)
+
+#estimate populations per province from world pop
+#for now, use china statistics bureau data
+pop <- read.csv("../data/context/pop2.csv", header = TRUE, colClasses = c(popper10k = "numeric"))
+datcsv <- merge(tacsv, pop, by = "prov", all = TRUE)
+datcsv$popdensity <- datcsv$popper10k*10000/datcsv$area
+
+write.csv(datcsv, file = "../data/provincedat.csv")
+
